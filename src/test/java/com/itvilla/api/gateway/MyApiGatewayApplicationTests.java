a0325592dd260c3515936f91c4c267f4df29a8fe
Narendar <Narendar@DESKTOP-UCU9CIL>
2019-05-13 20:20:39 +0530

    part1

---
+++ src/test/java/com/itvilla/api/gateway/MyApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itvilla.api.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
